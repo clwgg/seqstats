7747700525b92b3597c2148a9eda1ab533f02f46
clwgg <clw.genomics@gmail.com>
2025-08-12 09:08:04 -0700

    improved input validation to catch test cases

--- Makefile
+++ Makefile
@@ -3,10 +3,14 @@ CFLAGS ?= -O3 -Wall
 LDFLAGS ?= -lz
 
 SRC := seqstats.c
-DEPS := klib/kseq.h klib/ksort.h klib/khash.h klib/kvec.h
+DEPS := klib/kseq.h klib/ksort.h klib/khash.h klib/kvec.h klib/ketopt.h
 
 seqstats: $(SRC) $(DEPS)
 	$(CC) $(CFLAGS) $(SRC) $(LDFLAGS) -o $@
+
+.PHONY: debug
+debug: $(SRC) $(DEPS)
+	$(CC) -g -O0 -Wall $(SRC) $(LDFLAGS) -o seqstats
 
 .PHONY: test
 test: seqstats
--- seqstats.c
+++ seqstats.c
@@ -10,6 +10,7 @@
 #include "klib/ksort.h"
 #include "klib/khash.h"
 #include "klib/kvec.h"
+#include "klib/ketopt.h"
 
 // initialize kseq
 KSEQ_INIT(gzFile, gzread)
@@ -28,21 +29,38 @@ int main(int argc, char *argv[])
   int l;
   uint64_t n = 0;
   double full = 0;
-  if (argc == 1) {
-    printf("Usage: %s <in.fasta|in.fastq>\n", argv[0]);
+  int debug = 0;
+  
+  // Parse command line options
+  ketopt_t o = KETOPT_INIT;
+  ko_longopt_t longopts[] = {
+    { "debug", ko_no_argument, 'd' },
+    { 0, 0, 0 }
+  };
+  int c;
+  while ((c = ketopt(&o, argc, argv, 1, "d", longopts)) >= 0) {
+    if (c == 'd') debug = 1;
+  }
+  
+  if (debug) {
+    printf("DEBUG: Debug flag enabled\n");
+  }
+
+  if (o.ind == argc) {
+    printf("Usage: %s [--debug] <in.fasta|in.fastq>\n", argv[0]);
     return 1;
   }
-  else if (argc > 2) {
-    printf("\n\tPlease use\n\n\tcat in1.fa/q[.gz] in2.fa/q[.gz] | %s -", argv[0]);
+  else if (o.ind > argc - 1) {
+    printf("\n\tPlease use\n\n\tcat in1.fa/q[.gz] in2.fa/q[.gz] | %s [--debug] -", argv[0]);
     printf("\n\n\t          to specify more than one input.\n\n");
     return 1;
   }
 
-  if (strcmp( argv[1], "-") == 0) {
-    argv[1] = "/dev/stdin";
+  if (strcmp( argv[o.ind], "-") == 0) {
+    argv[o.ind] = "/dev/stdin";
   }
 
-  fp = gzopen(argv[1], "r");
+  fp = gzopen(argv[o.ind], "r");
 
   if (!fp) {
     printf("Can't open input file.\n\n");
@@ -63,9 +81,31 @@ int main(int argc, char *argv[])
   long min_len = LONG_MAX;
   long max_len = LONG_MIN;
 
-  while ((l = kseq_read(seq)) >= 0) {
+  while ((l = kseq_read(seq)) != -1) {
+    // Debug output
+    if (debug) {
+      printf("DEBUG: kseq_read returned %d, name: '%s' (%zu), seq: '%s' (%zu), qual: '%s' (%zu)\n", 
+             l, seq->name.s, seq->name.l, seq->seq.s, seq->seq.l, 
+             seq->qual.s ? seq->qual.s : "(null)", seq->qual.s ? seq->qual.l : 0);
+    }
 
     if (l == 0) continue;
+
+    // Check for kseq errors (negative return values)
+    if (l < 0) {
+      if (l == -2) {
+        printf("\n\tIncomplete FASTQ quartet detected. Each sequence must have 4 lines: header, sequence, '+', quality.\n\n");
+      } else {
+        printf("\n\tError reading sequence file (kseq error code: %d).\n\n", l);
+      }
+      goto validation_error;
+    }
+
+    // Validate FASTQ quality string length matches sequence length
+    if (seq->qual.s != NULL && seq->qual.l != seq->seq.l) {
+      printf("\n\tFASTQ quality string length (%zu) does not match sequence length (%zu).\n\n", seq->qual.l, seq->seq.l);
+      goto validation_error;
+    }
 
     if ((long)l < min_len) min_len = (long)l;
     if ((long)l > max_len) max_len = (long)l;
@@ -83,7 +123,7 @@ int main(int argc, char *argv[])
     full = full + l;
     n++;
   }
-  if (n == 0) goto seqerr;
+  if (n == 0) goto empty_file_error;
 
   // collect unique lengths and sort
   kvec_t(long) uniq; kv_init(uniq);
@@ -91,7 +131,7 @@ int main(int argc, char *argv[])
     if (!kh_exist(counts, i_k)) continue;
     kv_push(long, uniq, (long)kh_key(counts, i_k));
   }
-  if (kv_size(uniq) == 0) goto seqerr;
+  if (kv_size(uniq) == 0) goto empty_file_error;
   ks_introsort_long(kv_size(uniq), uniq.a);
 
   double adding = 0;
@@ -137,11 +177,16 @@ int main(int argc, char *argv[])
   gzclose(fp);
   return 0;
 
-  seqerr:
+  empty_file_error:
     printf("\n\tSequence file is empty.\n\n");
+    goto cleanup;
+
+  validation_error:
+    goto cleanup;
+
+  cleanup:
     kseq_destroy(seq);
     if (counts) kh_destroy(len_count, counts);
     gzclose(fp);
     return 1;
-
 }
